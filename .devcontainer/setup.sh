00cb0363f5b0131e7a357a095bfe3f37087260fe
rue_a <arne.ruemmler@gmail.com>
2023-06-28 09:34:15 +0200

    update

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Example commands to be executed
-echo "Hello, World!"
-ls -l
-# ... additional commands
+sudo apt-get update
+git clone https://github.com/OCR-D/ocrd_all
+cd ocrd_all
+
+sudo make deps-ubuntu
+make all
+source ocrd_all/venv/bin/activate
